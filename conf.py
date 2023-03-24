58d2e872384ec8e778cc1b9e5ec79d517c89562c
Jefficus <jeff@smithicus.com>
2023-03-24 15:21:57 -0600

    Hiding tags used for time management.

--- conf.py
+++ conf.py
@@ -441,7 +441,7 @@ TAG_PATH = "tags"
 # If you do not want to display a tag publicly, you can mark it as hidden.
 # The tag will not be displayed on the tag list page and posts.
 # Tag pages will still be generated.
-HIDDEN_TAGS = ['mathjax']
+HIDDEN_TAGS = ['mathjax', 'today', 'thisweek', 'thismonth']
 
 # Only include tags on the tag list/overview page if there are at least
 # TAGLIST_MINIMUM_POSTS number of posts or more with every tag. Every tag
