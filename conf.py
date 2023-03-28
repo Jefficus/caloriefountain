33813ef0f720d78159e1a25d91e263622d2cada4
Jefficus <jeff@smithicus.com>
2023-03-28 11:31:45 -0600

    Adding www to SITE_URL in hopes that will satisfy the RSS feed readersthat complain about https on caloriefountain.org

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@ BLOG_AUTHOR = "Jefferson Smith"  # (translatable)
 BLOG_TITLE = "Project Calorie Fountain"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://caloriefountain.org/"
+SITE_URL = "https://www.caloriefountain.org/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = ""
